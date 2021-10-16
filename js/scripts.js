6eb7cdd94825880fcff5bcd09c71fc79fc938801
Inestremo <84517961+Inestremo@users.noreply.github.com>
2021-10-16 18:17:04 +0300

    кинотеатр4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
-const totalPriceTag = document.querySelector(".price-total")
+const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -11,3 +13,9 @@ schemeSvg.addEventListener("click", (event) => {
     console.log(schemeSvg.querySelectorAll(".active").length)
 }
 });
+
+menuButton.addEventListener('click', () => {
+
+  console.log("Кликнули по меню");
+  menu.classList.toggle("is-open");
+})
